26a51dcaf09193e52eb5ea0824a2b37df48cb7b7
guimoreira07 <gui.melo01@gmail.com>
2026-05-06 18:12:13 -0300

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,29 @@
+// const { getDefaultConfig } = require("expo/metro-config");
+// const { withNativeWind } = require("nativewind/metro");
+
+// const config = getDefaultConfig(__dirname);
+
+// module.exports = withNativeWind(config, {
+//   input: "./global.css",
+  // Force write CSS to file system instead of virtual modules
+  // This fixes iOS styling issues in development mode
+//   forceWriteFileSystem: true,
+// });
+
+
+const path = require("path");
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
-const config = getDefaultConfig(__dirname);
+const projectRoot = __dirname;
+
+const config = getDefaultConfig(projectRoot);
+
+config.watchFolders = [
+  ...(config.watchFolders || []),
+  path.resolve(projectRoot, "node_modules/react-native-css-interop/.cache"),
+];
 
 module.exports = withNativeWind(config, {
   input: "./global.css",
-  // Force write CSS to file system instead of virtual modules
-  // This fixes iOS styling issues in development mode
-  forceWriteFileSystem: true,
 });
